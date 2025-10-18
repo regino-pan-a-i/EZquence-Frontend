f81b16037fc41bd3d65e52e2a8e216de592ff924
Andre Regino <reginopanai@gmail.com>
2025-10-18 12:33:21 -0600

    Initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,35 @@
+import { login, signup } from '../../util/supabase/actions'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Login | EZquence",
+  description: "Login to your account on EZquence. Manage services, teams, and more.",
+  keywords: "",
+  robots: "noindex, nofollow"
+};
+
+export default function LoginPage() {
+
+  return (
+    <>
+      <div className='m-5'>
+      <h2 className="mb-4 text-2xl font-bold">Enter Credentials</h2>
+      <form className="p-8 mx-auto space-y-6 max-w-lg bg-white rounded-lg shadow-lg dark:bg-neutral-900">
+
+        <label className="mb-1 text-sm font-medium" htmlFor="email">Email:</label>
+        <input className="py-2 px-3 w-full rounded border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:outline-none dark:border-neutral-700" id="email" name="email" type="email" required />
+        <label className="mb-1 text-sm font-medium" htmlFor="password">Password:</label>
+        <input className="py-2 px-3 w-full rounded border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:outline-none dark:border-neutral-700" id="password" name="password" type="password" required />
+      
+        <button type="submit" className="cursor-pointer py-2 w-full font-semibold text-white bg-blue-600 rounded transition hover:bg-blue-700" formAction={login}>
+          Log in
+        </button>
+        {/* <button type="submit" className="cursor-pointer py-2 w-full font-semibold text-white bg-gray-600 rounded transition hover:bg-gray-700" formAction={signup}>
+          Sign up
+        </button> */}
+      </form>
+      </div>
+
+    </>
+  )
+}
