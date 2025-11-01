df310f3ad618ecad25b5e528e26d47aa97032e27
Andre Regino <reginopanai@gmail.com>
2025-10-31 18:12:25 -0600

    Added transtack provider for useQuery react hook

--- src/app/(protected)/admin/dashboard/page.tsx
+++ src/app/(protected)/admin/dashboard/page.tsx
@@ -1,50 +1,127 @@
-import ScoreCard from '@/components/scorecard/ScoreCard'
-import { FaDollarSign, FaReceipt, FaUser } from "react-icons/fa"
-import { DateFilter } from '@/components/filters';
-
-
+'use client';
+import ScoreCard from '@/components/scorecard/ScoreCard';
+import { FaDollarSign, FaReceipt, FaUser } from 'react-icons/fa';
+import DateFilter from '@/components/filters/DateFilter';
+import { useQuery } from '@tanstack/react-query';
+import { Order, OrderResponse, OrderStatus } from '@/utils/supabase/schema';
+import { useState } from 'react';
+import { supabase } from '@/utils/supabase/supabaseClient';
 
 export default function DashboardPage() {
+  const [dateRange, setDateRange] = useState({
+    start: new Date().toISOString().split('T')[0], // Today's date by default
+    end: new Date().toISOString().split('T')[0], // Today's date by default
+  });
+  const handleDateRangeChange = (start: string, end: string) => {
+    setDateRange({ start, end });
+  };
 
+  const { data: orders, isLoading } = useQuery<OrderResponse>({
+    queryKey: ['orders', dateRange.start, dateRange.end],
+    queryFn: async () => {
+      console.log('fetching');
 
-  const TotalRevenue = () => {
-    
-  }
-  
-  const Orders = ()=> {
+      // Get the session token
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      const token = session?.access_token;
+      const res = await fetch(
+        `http://localhost:8080/order/daterange?start=${dateRange.start}&end=${dateRange.end}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      if (!res.ok) console.log('Failed to fetch orders', res);
+      return res.json();
+    },
+  });
 
-  }
+  const OrdersTotal = (orderList: OrderResponse) => {
+    return orderList.data.length;
+  };
 
-  const AvgOrderValue = () => {
-  
-  }
+  const TotalRevenue = (orderList: OrderResponse) => {
+    let orderData = orderList.data;
+    return orderData
+      .map((order: Order) => {
+        if (order.status === OrderStatus.COMPLETED) return order.orderTotal;
+        return 0;
+      })
+      .reduce((a, b) => a + b, 0);
+  };
 
-  const PendingOrders = () => {
+  const AvgOrderValue = (orderList: OrderResponse) => {
+    let orderData = orderList.data;
+    const total = orderData
+      .map((order: Order) => {
+        return order.orderTotal;
+      })
+      .reduce((a, b) => a + b, 0);
+    return total / orderData.length;
+  };
 
-  }
+  const PendingOrders = (orderList: OrderResponse) => {
+    let orderData = orderList.data;
+    return orderData.filter((order: Order) => {
+      return (
+        order.status === OrderStatus.IN_PROGRESS ||
+        order.status === OrderStatus.PAID ||
+        order.status === OrderStatus.DELAYED
+      );
+    }).length;
+  };
 
   return (
     <>
       <div className="flex flex-row m-4 justify-between">
         <h1 className="text-2xl font-bold">Dashboard</h1>
-
-        <DateFilter 
+        <DateFilter
           type="range"
-        />
-
-
-      </div>   
-      <div className="w-full">
-        <ScoreCard
-            title="Key Primary Indicators"
-            data={[
-              { value: '$1,234', label: 'Total Revenue', color: 'green', icon: <FaDollarSign /> },
-              { value: '456', label: 'Orders', color: 'yellow', icon: <FaReceipt /> },
-              { value: '$1,234', label: 'Avg Order Value', color: 'green', icon: <FaReceipt /> },
-              { value: '456', label: 'Pending Orders', color: 'green', icon: <FaReceipt /> }
-            ]}
+          onDateRangeChange={handleDateRangeChange}
+          defaultStartDate={dateRange.start}
+          defaultEndDate={dateRange.end}
         />
       </div>
+
+      <div className="w-full">
+        {orders && (
+          <ScoreCard
+            title="Key Primary Indicators"
+            data={[
+              {
+                value: `${TotalRevenue(orders)}`,
+                label: 'Total Revenue',
+                color: 'green',
+                icon: <FaDollarSign />,
+              },
+              {
+                value: `${OrdersTotal(orders)}`,
+                label: 'Orders',
+                color: 'yellow',
+                icon: <FaReceipt />,
+              },
+              {
+                value: `${AvgOrderValue(orders) || 0}`,
+                label: 'Avg Order Value',
+                color: 'green',
+                icon: <FaReceipt />,
+              },
+              {
+                value: `${PendingOrders(orders)}`,
+                label: 'Pending Orders',
+                color: 'green',
+                icon: <FaReceipt />,
+              },
+            ]}
+          />
+        )}
+      </div>
+
+      <div>{JSON.stringify(orders)}</div>
     </>
-  )
+  );
 }
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,15 +1,12 @@
 import Sidebar from '@/components/sidebar/Sidebar';
+import { TanstackProvider } from '@/components/providers/tanstack-provider';
 
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen">
       <Sidebar variant="admin" />
       <main className="flex-1 overflow-auto m-4">
-        {children}
+        <TanstackProvider>{children}</TanstackProvider>
       </main>
     </div>
   );
