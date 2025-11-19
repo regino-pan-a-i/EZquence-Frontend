088c1f941cabd35176b86a194b01c5fadf4979d2
Andre Regino <reginopanai@gmail.com>
2025-11-19 15:17:06 -0700

    added toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { SupabaseProvider } from './supabase-provider';
+import { Toaster } from 'react-hot-toast';
 import './globals.css';
 
 const geistSans = Geist({
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Toaster position="top-center" />
         <SupabaseProvider>{children}</SupabaseProvider>
       </body>
     </html>
